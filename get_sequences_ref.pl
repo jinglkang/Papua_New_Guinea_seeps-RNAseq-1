41fe74a19dbfdee9312b4433a45eb9eeef152ac3
jinglkang <45533802+jinglkang@users.noreply.github.com>
2020-02-11 22:46:40 +0800

    Create get_sequences_ref.pl
    get reference per species

---
+++ get_sequences_ref.pl
@@ -0,0 +1,45 @@
+#!/usr/bin/perl -w
+use File::Basename;
+use Getopt::Long;
+my $opt = GetOptions( 'input:s', \$blast_result,
+        'nuc:s', \$nuc,
+        'output:s', \$output);
+if (!($opt && $blast_result && $nuc)) {
+    print STDERR "\nExample usage:\n";
+    print STDERR "\n$0 -input=\"blast_result diretory\" /
+    -nuc=\"nucleotide sequeces directory\" /
+    -output=\"final reference sequences direcotry\"\n\n";
+    exit;
+}
+my @fas=<$nuc/*fa>;
+my %seq;
+my ($spe,$tran);
+foreach my $fa (@fas) {
+        open "fil", "$fa";
+        my $spec=basename($fa);
+        (my $spe)=$spec=~/(.*)\_nuc\.fa/;
+        while (<fil>) {
+                chomp;
+                if (/>/) {
+                        s/>//;
+                        $tran=$_;
+                } else {
+                        $seq{$spe}->{$tran}.=$_ if defined $tran;
+                }
+        }
+}
+my @blasts=<$blast_result/*result>;
+mkdir $output;
+foreach my $blast (@blasts) {
+        open "fil", "$blast";
+        while (<fil>) {
+                chomp;
+                my @a=split;
+                (my $spec)=$a[1]=~/(.*)\_\d+/;
+                $a[1]=~s/\s+$//;
+                my $ref=$spec.".fa";
+                open $spec, ">>$output/$ref";
+                my $info="$a[0]"."\t"."$a[1]";
+                print $spec ">$info\n$seq{$spec}->{$a[1]}\n" if defined $seq{$spec}->{$a[1]};
+        }
+}
