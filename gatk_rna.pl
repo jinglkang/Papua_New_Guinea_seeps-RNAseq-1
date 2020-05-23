240f06098d34d1dc8a361b40e0ec17b78cfa31d6
jinglkang <45533802+jinglkang@users.noreply.github.com>
2020-05-23 18:01:28 +0800

    Update gatk_rna.pl

--- gatk_rna.pl
+++ gatk_rna.pl
@@ -39,40 +39,40 @@ foreach (@bam) {
         my $split=$name.".split.bam"; # SplitNCigarReads
         my $vcf=$name.".vcf"; my $vcf_idx=$vcf.".idx"; # GATK Haplotype
         my $snp_vcf=$name.".snp.vcf"; my $snp_vcf_idx=$snp_vcf.".idx";
-        if (-e $snp_vcf) {
+        if (-e "./$output/$snp_vcf") {
                 print "congrat, $name already has snp file! let's start the next one.\n";
                 `mv $vcf $vcf_idx $snp_vcf $snp_vcf_idx $output`;
-                `rm $bam_sort $bam_sort_add $bam_sort_add_dup $resort`;
+#               `rm $bam_sort $bam_sort_add $bam_sort_add_dup $resort`;
                 next;
         } else {
-                `java -jar $PICARD SortSam INPUT=$input_1 OUTPUT=$bam_sort SO=coordinate`; # sortsam
+                `java -jar -Djava.io.tmpdir=./tmp $PICARD SortSam INPUT=$input_1 OUTPUT=$bam_sort SO=coordinate`; # sortsam
                 if (-e $bam_sort) {
-                        `java -jar $PICARD AddOrReplaceReadGroups INPUT=$bam_sort OUTPUT=$bam_sort_add SO=coordinate RGID=Label RGLB=Label RGPL=illumina RGPU=Label RGSM=Label`; # add read group
+                        `java -jar -Djava.io.tmpdir=./tmp $PICARD AddOrReplaceReadGroups INPUT=$bam_sort OUTPUT=$bam_sort_add SO=coordinate RGID=Label RGLB=Label RGPL=illumina RGPU=Label RGSM=Label`; # add read group
                 } else {
                         die "There is no $bam_sort, error occurs in the first step: SortSam\n";
                 }
                 if (-e $bam_sort_add) {
-                        `java -jar $PICARD MarkDuplicates INPUT=$bam_sort_add OUTPUT=$bam_sort_add_dup M=$dump CREATE_INDEX=true`; # mark duplicated reads
+                        `java -jar -Djava.io.tmpdir=./tmp $PICARD MarkDuplicates INPUT=$bam_sort_add OUTPUT=$bam_sort_add_dup M=$dump CREATE_INDEX=true`; # mark duplicated reads
                 } else {
                         die "This is no $bam_sort_add, error occurs in the second step: AddOrReplaceReadGroups\n";
                 }
                 if (-e $bam_sort_add_dup) {
-                        `java -jar $PICARD ReorderSam INPUT=$bam_sort_add_dup OUTPUT=$resort SEQUENCE_DICTIONARY= $DIC CREATE_INDEX=TRUE`; # resorted
+                        `java -jar -Djava.io.tmpdir=./tmp $PICARD ReorderSam INPUT=$bam_sort_add_dup OUTPUT=$resort SEQUENCE_DICTIONARY= $DIC CREATE_INDEX=TRUE`; # resorted
                 } else {
                         die "There is no $bam_sort_add_dup, error occurs in the third step: MarkDuplicates\n";
                 }
                 if (-e $resort) {
-                        `java -jar $GATK -T SplitNCigarReads -R $GATKREF -I $resort -o $split -rf ReassignOneMappingQuality -RMQF 255 -RMQT 60 --filter_reads_with_N_cigar`; # SplitNCigarReads
+                        `java -jar -Djava.io.tmpdir=./tmp $GATK -T SplitNCigarReads -R $GATKREF -I $resort -o $split -rf ReassignOneMappingQuality -RMQF 255 -RMQT 60 --filter_reads_with_N_cigar`; # SplitNCigarReads
                 } else {
                         die "There is no $resort, error occurs in the fourth step: ReorderSam\n";
                 }
                 if (-e $split) {
-                        `java -jar $GATK -T HaplotypeCaller -R $GATKREF -I $split -o $vcf`; # GATK Haplotype
+                        `java -jar -Djava.io.tmpdir=./tmp $GATK -T HaplotypeCaller -R $GATKREF -I $split -o $vcf`; # GATK Haplotype
                 } else {
                         die "There is no $split, error occurs in the fifth step: SplitNCigarReads\n";
                 }
                 if (-e $vcf) {
-                        `java -jar $GATK -T SelectVariants -selectType SNP -R $GATKREF -selectType MNP -V $vcf -o $snp_vcf`; # SelectVariants
+                        `java -jar -Djava.io.tmpdir=./tmp $GATK -T SelectVariants -selectType SNP -R $GATKREF -selectType MNP -V $vcf -o $snp_vcf`; # SelectVariants
                 } else {
                         die "There is no $vcf, error occurs in the sixth step: HaplotypeCaller\n";
                 }
