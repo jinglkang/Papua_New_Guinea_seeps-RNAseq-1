f8b484b09966d4da88f6b1472e14387ebce95e53
jinglkang <45533802+jinglkang@users.noreply.github.com>
2020-05-29 14:15:26 +0800

    Update vcf_bayescan.pl

--- vcf_bayescan.pl
+++ vcf_bayescan.pl
@@ -6,12 +6,13 @@ use File::Basename;
 use Getopt::Long;
 
 my $opt = GetOptions( 'vcf:s', \ my $vcf,
-    'pop_def:s', \ my $pop_def
+    'pop_def:s', \ my $pop_def,
+    'id_correlation_num:s', \my $cor_tab
     );
 
-if (!($vcf && $pop_def)) {
+if (!($vcf && $pop_def && $cor_tab)) {
     print STDERR "\nExample usage:\n";
-    print STDERR "\n$0 --vcf \"vcf file\" --pop_def \"the sample correlation with population\"\n\n";
+    print STDERR "\n$0 --vcf \"vcf file\" --pop_def \"the sample correlation with population\" --id_correlation_num \"correlation table output\"\n\n";
     exit;
 }
 
@@ -22,78 +23,129 @@ open VCF, "$vcf" or die "cannot open $vcf\n";
 while (<VCF>) {
         chomp;
         if (/^##GATKCommandLine/) {
-                my $variant="";
-                ($variant)=$_=~/variant=(\[.*\])\s+out\=/;
-                my @variant=split /\,/, $variant if $variant;
-                my $i=0;
-                foreach my $var (@variant) {
-                        $i++;
-                        (my $sample)=$var=~/name=(.*)\ssource=/;
-                        $sample=~s/^\s+//;
-                        $sample=~s/\s+$//;
-                        $hash{$i}=$sample;
-                }
+            my $variant="";
+            ($variant)=$_=~/variant=(\[.*\])\s+out\=/;
+            my @variant=split /\,/, $variant if $variant;
+            my $i=0;
+            foreach my $var (@variant) {
+                $i++;
+                (my $sample)=$var=~/name=(.*)\ssource=/;
+                $sample=~s/^\s+//;
+                $sample=~s/\s+$//;
+                $hash{$i}=$sample;
+            }
         }
         if (/^#/) {
-                next;
+            next;
         } elsif (/PASS/) {
-                my @a=split;
-                my $loci=$a[0]."_".$a[1];
-                push @loci, $loci;
-                my $j=0;
-                for (my $i = -19; $i <= -1; $i++) {
-                        $j++;
-                        my @b=split /\:/, $a[$i];
-                        $genetype{$hash{$j}}->{$loci}=$b[0];
-                }
+            my @a=split;
+            my $loci=$a[0]."_".$a[1];
+            push @loci, $loci;
+            my $j=0;
+            for (my $i = -19; $i <= -1; $i++) {
+                $j++;
+                my @b=split /\:/, $a[$i];
+                $genetype{$hash{$j}}->{$loci}=$b[0];
+            }
         }
 }
+
+my $loci_num=@loci;
+print "loci]=$loci_num\n\n";
 
 my %pop=();
-open POP, "$pop_def";
+my @sample=();
+my %allel_type=();
+my %allel_num=();
+open POP, "$pop_def" or die "cannot open $pop_def\n";
 while (<POP>) {
-        chomp;
-        s/^\s+//;
-        s/\s+$//;
-        my @a=split;
-        $pop{$a[1]}.=$a[0]."\t";
+    chomp;
+    s/^\s+//;
+    s/\s+$//;
+    my @a=split;
+    push @sample, $a[0];
+    $pop{$a[1]}.=$a[0]."\t";
 }
 
+my $pop_num=scalar(keys %pop);
+print "[populations]=$pop_num\n\n";
+
+# get the total genetype of each loci in all individuals
+&get_allel_from_all_sample();
+
+my @samp_pop;
+my $gene_num;
 foreach my $pop (keys %pop) {
-        $pop{$pop}=~s/\s+$//;
-        print "[population]= $pop\t$pop{$pop}\n";
-        my @sample=split /\t/, $pop{$pop};
-        my $gene_num=2*scalar(@sample);
-        foreach my $loci (@loci) {
-                my %hash2=();
-                my $info;
-                foreach my $sample (@sample) {
-                        my $gtype;
-                        $gtype=$genetype{$sample}->{$loci};
-                        if ($hash2{$gtype} && $hash2{$gtype}>=1) {
-                                $hash2{$gtype}++;
-                        } elsif (!$hash2{$gtype}) {
-                                $hash2{$gtype}=1;
-                        }
-                }
+    my $i++;
+    $pop{$pop}=~s/\s+$//;
+    $pop{$pop}=~s/^\s+//;
+    print "[pop]=$i\n";
+    @samp_pop=split /\t/, $pop{$pop};
+    $gene_num=2*@samp_pop;
+    &write_info_according_loci();
+}
 
-                my %allel_num=();
-                foreach my $key (sort keys %hash2) {
-                        my @a=split /\//, $key;
-                        for (my $i = 0; $i < 2; $i++) {
-                                if ($allel_num{$a[$i]}) {
-                                        $allel_num{$a[$i]}+=$hash2{$key};
-                                } elsif (!$allel_num{$a[$i]}) {
-                                        $allel_num{$a[$i]}=$hash2{$key};
-                                }
-                        }
-                }
 
-                my $allel_total_type=scalar(keys %allel_num);
-                foreach my $key (sort {$a<=>$b} keys %allel_num) {
-                        $info.=$allel_num{$key}."\t";
-                }
-                $info=~s/\s+$//;
-                print "$loci $gene_num $allel_total_type $info\n";
+sub get_allel_from_all_sample {
+    my @gtype;
+    # get all allel types of all sample in each loci
+    foreach my $loci (@loci) {
+        my %hash;
+        foreach my $sample (@sample) {
+            my $gtype;
+            $gtype=$genetype{$sample}->{$loci};
+            my @allel=split /\//, $gtype;
+            foreach my $allel (@allel) {
+                if ($hash{$allel} && $hash{$allel} >1) {
+                    $hash{$allel}++;
+                } elsif (!$hash{$allel}) {$hash{$allel}=1}
+            }
         }
+        my @allel_type=sort {$a<=>$b} keys %hash;
+        @{$allel_type{$loci}}=@allel_type;
+    }
+}
+
+sub write_info_according_loci {
+    my $i=0;
+    foreach my $loci (@loci) {
+        $i++;
+        my %hash2=();
+        foreach my $sample (@samp_pop) {
+            my $gtype;
+            $gtype=$genetype{$sample}->{$loci};
+            if ($hash2{$gtype} && $hash2{$gtype}>=1) {
+                $hash2{$gtype}++;
+            } elsif (!$hash2{$gtype}) {
+                $hash2{$gtype}=1;
+            }
+        }
+
+        my %allel_num=();
+        foreach my $key (sort keys %hash2) {
+            my @a=split /\//, $key;
+            for (my $i = 0; $i < @a; $i++) {
+                if ($allel_num{$a[$i]}) {
+                    $allel_num{$a[$i]}+=$hash2{$key};
+                } elsif (!$allel_num{$a[$i]}) {
+                    $allel_num{$a[$i]}=$hash2{$key};
+                }
+            }
+        }
+
+        my $allel_total_type=@{$allel_type{$loci}};
+        my @allel=@{$allel_type{$loci}};
+        my $info="";
+        foreach my $allel (@allel) {
+            if ($allel_num{$allel}) {
+                $info.=$allel_num{$allel}." ";
+                } elsif (! $allel_num{$allel}) {
+                    $info="0";
+                }
+        }
+        $info=~s/\s+$//;
+        print "$i $gene_num $allel_total_type $info\n";
+        open COR, ">>$cor_tab";
+        print COR "$loci $i\n";
+    }
 }
